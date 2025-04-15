949a2346d8dd1bc00fafe1a9b694336e09d473e8
alina <alina4268@mail.ru>
2025-04-15 11:57:03 +0200

    add experiments

--- run_cifar.sh
+++ run_cifar.sh
@@ -5,4 +5,6 @@ python quant_resnet.py --dataset cifar --quant_type none --epochs 10
 python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 8 --epochs 10
 python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 6 --epochs 10
 python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 4 --epochs 10
-python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 8 --epochs 1 
+python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 8 --epochs 1
+python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 6 --epochs 1
+python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 4 --epochs 1
