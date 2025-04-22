aa99de1e24d18a366f9bd3948b3d64c29c7b1ec4
Alina Burykina <aburykina@constructor.university>
2025-04-22 21:52:21 +0000

    update

--- run_cifar.sh
+++ run_cifar.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Run only CIFAR experiments
-python quant_resnet.py --dataset cifar --quant_type none --epochs 50
-python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 8 --epochs 50
-python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 6 --epochs 50
-python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 4 --epochs 50
-python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 8 --epochs 1
-python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 6 --epochs 1
-python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 4 --epochs 1
+#python quant_resnet.py --dataset cifar --quant_type none --epochs 50
+python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 8 --epochs 20
+python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 6 --epochs 20
+python quant_resnet.py --dataset cifar --quant_type qat --bitwidth 4 --epochs 20
+#python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 8 --epochs 1
+#python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 6 --epochs 1
+#python quant_resnet.py --dataset cifar --quant_type ptq --bitwidth 4 --epochs 1
--- utils/quantization.py
+++ utils/quantization.py
@@ -3,6 +3,7 @@ import torch
 def get_qconfig_for_bitwidth(bitwidth):
     if bitwidth == 8:
         act_qconfig = torch.ao.quantization.FakeQuantize.with_args(
+            #MinMaxObserver посмотреть habr
             observer=torch.ao.quantization.observer.HistogramObserver.with_args(
                 quant_min=0,
                 quant_max=255,
