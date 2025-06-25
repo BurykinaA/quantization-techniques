3f74cad376f34d738cf49e9ae706356cb0157908
Alina Burykina <alina4268@mail.ru>
2025-06-25 05:52:54 +0200

    fix

--- ADC/bert_experiments/bert_experiment_setup.py
+++ ADC/bert_experiments/bert_experiment_setup.py
@@ -59,15 +59,8 @@ def get_squad_dataloaders(batch_size=16, subset_size=None):
 
     tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
 
-    # The preprocessing function needs to be applied in a batched way to leverage the tokenizer's speed
     def prepare_train_features(examples):
-        # Some of the questions have lots of whitespace on the left, which is not useful and will make the
-        # truncation of the context fail (the tokenized question will take a lots of space). So we remove that
-        # left whitespace
         examples["question"] = [q.lstrip() for q in examples["question"]]
-
-        # Tokenize our examples with truncation and padding, but keep the overflows using a stride. This results
-        # in one example possible giving several features, each of length max_length, it's a sliding window approach.
         tokenized_examples = tokenizer(
             examples["question"],
             examples["context"],
@@ -79,79 +72,71 @@ def get_squad_dataloaders(batch_size=16, subset_size=None):
             padding="max_length",
         )
 
-        # Since one example might give us several features if it has a long context, we need a map from a feature
-        # to its corresponding example. This key gives us just that.
         sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
-        # The offset mappings will give us a map from token to character position in the original context. This will
-        # help us compute the start_positions and end_positions.
         offset_mapping = tokenized_examples.pop("offset_mapping")
 
-        # Let's label those examples!
         tokenized_examples["start_positions"] = []
         tokenized_examples["end_positions"] = []
-        tokenized_examples["example_id"] = []
 
         for i, offsets in enumerate(offset_mapping):
-            # We will label impossible answers with the index of the CLS token.
             input_ids = tokenized_examples["input_ids"][i]
             cls_index = input_ids.index(tokenizer.cls_token_id)
-
-            # Grab the sequence corresponding to that example (to know what is the context and what is the question).
             sequence_ids = tokenized_examples.sequence_ids(i)
-
-            # One example can give several spans, this is the index of the example containing this span of text.
             sample_index = sample_mapping[i]
             answers = examples["answers"][sample_index]
-            tokenized_examples["example_id"].append(examples["id"][sample_index])
-
-            # If no answers are given, set the cls_index as answer.
+            
             if len(answers["answer_start"]) == 0:
                 tokenized_examples["start_positions"].append(cls_index)
                 tokenized_examples["end_positions"].append(cls_index)
             else:
-                # Start/end character index of the answer in the text.
                 start_char = answers["answer_start"][0]
                 end_char = start_char + len(answers["text"][0])
-
-                # Start token index of the current span in the text.
                 token_start_index = 0
                 while sequence_ids[token_start_index] != 1:
                     token_start_index += 1
-
-                # End token index of the current span in the text.
                 token_end_index = len(input_ids) - 1
                 while sequence_ids[token_end_index] != 1:
                     token_end_index -= 1
-
-                # Detect if the answer is out of the span (in which case this feature is labeled with the CLS index).
                 if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
                     tokenized_examples["start_positions"].append(cls_index)
                     tokenized_examples["end_positions"].append(cls_index)
                 else:
-                    # Otherwise move the token_start_index and token_end_index to the two ends of the answer.
-                    # Note: we could go after the last token of the context if the answer is the last word
                     while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
                         token_start_index += 1
                     tokenized_examples["start_positions"].append(token_start_index - 1)
                     while offsets[token_end_index][1] >= end_char:
                         token_end_index -= 1
                     tokenized_examples["end_positions"].append(token_end_index + 1)
-
         return tokenized_examples
 
-    tokenized_datasets = datasets.map(prepare_train_features, batched=True, remove_columns=datasets["train"].column_names)
-    
-    train_dataset = tokenized_datasets["train"]
-    eval_dataset = tokenized_datasets["validation"]
+    def prepare_validation_features(examples):
+        examples["question"] = [q.lstrip() for q in examples["question"]]
+        tokenized_examples = tokenizer(
+            examples["question"],
+            examples["context"],
+            truncation="only_second",
+            max_length=MAX_LENGTH,
+            stride=DOC_STRIDE,
+            return_overflowing_tokens=True,
+            return_offsets_mapping=True,
+            padding="max_length",
+        )
+        sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+        tokenized_examples["example_id"] = [examples["id"][i] for i in sample_mapping]
+        return tokenized_examples
 
-    train_dataloader = DataLoader(train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size)
-    eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size)
+    # Create datasets for training and for evaluation where loss is computed
+    train_dataset = datasets["train"].map(prepare_train_features, batched=True, remove_columns=datasets["train"].column_names)
+    eval_dataset = datasets["validation"].map(prepare_train_features, batched=True, remove_columns=datasets["validation"].column_names)
     
-    # For evaluation, we need the raw validation set features
+    # Create the special dataset for post-processing that keeps offset_mapping
     validation_features = datasets["validation"].map(
-        prepare_train_features,
+        prepare_validation_features,
         batched=True,
         remove_columns=datasets["validation"].column_names
     )
 
+    train_dataloader = DataLoader(train_dataset, shuffle=True, collate_fn=default_data_collator, batch_size=batch_size)
+    eval_dataloader = DataLoader(eval_dataset, collate_fn=default_data_collator, batch_size=batch_size)
+    
     return train_dataloader, eval_dataloader, datasets["validation"], validation_features 
