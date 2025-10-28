562936d5785e4fe2be9ec6827e273c535eaaf115
Alina Burykina <alina4268@mail.ru>
2025-10-28 10:33:22 +0100

    QAT

--- ADC/bert_clean/run_adc_ptq_example.sh
+++ ADC/bert_clean/run_adc_ptq_example.sh
@@ -2,8 +2,8 @@
 # Example script to run ADC Post-Training Quantization (PTQ) with WandB logging and visualizations
 
 # Configuration
-QAT_CHECKPOINT="./ADC/bert_clean/checkpoints/outputs_qat/checkpoint-1000"  # Change this to your QAT checkpoint
-OUTPUT_DIR="./ADC/bert_clean/outputs_adc_ptq"
+QAT_CHECKPOINT="./ADC/bert_clean/checkpoints/outputs_qat/squad_qat_20251028_001954"  # Change this to your QAT checkpoint
+OUTPUT_DIR="./ADC/bert_clean/checkpoints/outputs_ad_ptq"
 
 # ADC Hardware Configuration
 BX=8              # Activation bits
